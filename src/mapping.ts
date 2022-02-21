bfbfb2da6df8a965860c58e7e7d5f86d3d506fd3
SyedMuqeetAqib <smuqeetaqib@gmail.com>
2022-02-21 16:47:06 +0500

    first commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   DaoStakeContract,
   BaseInterestUpdated,
@@ -6,33 +6,17 @@ import {
   Paused,
   StakeCompleted,
   Unpaused,
-  Unstake
-} from "../generated/DaoStakeContract/DaoStakeContract"
-import { ExampleEntity } from "../generated/schema"
+  Unstake,
+} from "../generated/DaoStakeContract/DaoStakeContract";
+import { Transaction, User, StakeDetail } from "../generated/schema";
 
 export function handleBaseInterestUpdated(event: BaseInterestUpdated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._newRate = event.params._newRate
-  entity._oldRate = event.params._oldRate
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.from = event.address;
+  transaction.method = "HandleBaseInterestUpdate";
+  transaction.time = event.block.timestamp;
+  transaction.blockNumber = event.block.number;
+  transaction.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -63,12 +47,109 @@ export function handleBaseInterestUpdated(event: BaseInterestUpdated): void {
   // - contract.unstakeALT(...)
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.from = event.params.newOwner;
+  transaction.method = "OwnershipTransferred";
+  transaction.time = event.block.timestamp;
+  transaction.blockNumber = event.block.number;
+  transaction.save();
+}
 
-export function handlePaused(event: Paused): void {}
+export function handlePaused(event: Paused): void {
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.from = event.params.account;
+  transaction.method = "Pause";
+  transaction.time = event.block.timestamp;
+  transaction.blockNumber = event.block.number;
+  transaction.save();
+}
 
-export function handleStakeCompleted(event: StakeCompleted): void {}
+export function handleStakeCompleted(event: StakeCompleted): void {
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.from = event.params.staker;
+  transaction.method = "StakeComplete";
+  transaction.amount = event.params.rewardAmount;
+  transaction.time = event.block.timestamp;
+  transaction.blockNumber = event.block.number;
+  transaction.save();
 
-export function handleUnpaused(event: Unpaused): void {}
+  let id = event.params.staker.toHexString();
+  let user = User.load(id);
+  if (!user) {
+    user = new User(id);
+    user.address = event.params.staker;
+    user.stakedAmount = event.params.rewardAmount;
+    user.save();
+  } else {
+    user.stakedAmount = user.stakedAmount.plus(event.params.rewardAmount);
+    user.save();
+  }
+  let stakeDetails = StakeDetail.load(
+    Address.fromString(
+      "0xFa70F492D9f4fc28C8D6b9e65eac0B0AA363AF7F"
+    ).toHexString()
+  );
+  if (!stakeDetails) {
+    stakeDetails = new StakeDetail(
+      Address.fromString(
+        "0xFa70F492D9f4fc28C8D6b9e65eac0B0AA363AF7F"
+      ).toHexString()
+    );
+    stakeDetails.totalStakedAmount = event.params.altQuantity;
+    stakeDetails.totalRewardAmount = event.params.rewardAmount;
+    stakeDetails.save();
+  } else {
+    stakeDetails.totalStakedAmount = stakeDetails.totalStakedAmount.plus(
+      event.params.altQuantity
+    );
+    stakeDetails.totalRewardAmount = stakeDetails.totalRewardAmount.plus(event.params.rewardAmount);
+    stakeDetails.save();
+  }
+}
 
-export function handleUnstake(event: Unstake): void {}
+export function handleUnpaused(event: Unpaused): void {
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.from = event.params.account;
+  transaction.method = "UnPause";
+  transaction.time = event.block.timestamp;
+  transaction.blockNumber = event.block.number;
+  transaction.save();
+}
+
+export function handleUnstake(event: Unstake): void {
+  let transaction = new Transaction(event.transaction.hash.toHex());
+  transaction.from = event.params.staker;
+  transaction.method = "UnStake";
+  transaction.amount = event.params.altQuantity;
+  transaction.time = event.block.timestamp;
+  transaction.blockNumber = event.block.number;
+  transaction.save();
+
+  let id = event.params.staker.toHexString();
+  let user = User.load(id);
+
+  if (user) {
+    user.stakedAmount = event.params.altQuantity;
+    user.save();
+  }
+
+  let stakeDetails = StakeDetail.load(
+    Address.fromString(
+      "0xFa70F492D9f4fc28C8D6b9e65eac0B0AA363AF7F"
+    ).toHexString()
+  );
+  if (stakeDetails) {
+    // stakeDetails = new StakeDetail(
+    //   Address.fromString(
+    //     "0xFa70F492D9f4fc28C8D6b9e65eac0B0AA363AF7F"
+    //   ).toHexString()
+    // );
+    // stakeDetails.totalStakedAmount = event.params.altQuantity;
+    // stakeDetails.totalRewardAmount = BigInt.fromI64(0);
+    // stakeDetails.save();
+    stakeDetails.totalStakedAmount = stakeDetails.totalStakedAmount.minus(event.params.altQuantity);
+    stakeDetails.totalRewardAmount = stakeDetails.totalRewardAmount;
+    stakeDetails.save();
+  }
+}
